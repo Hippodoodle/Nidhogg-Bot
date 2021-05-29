aeb2064f443872a0516a530beaa8c5f749dd04da
Thomas McCausland <2472525m@student.gla.ac.uk>
2021-05-29 18:37:37 +0200

    Refactored counting cog to reduce complexity

--- cogs/counting.py
+++ cogs/counting.py
@@ -19,46 +19,45 @@ class Counting(commands.Cog):
         """
 
         # Only checks messages in counting-channel
-        if ctx.channel.id == 699762298721665158 or ctx.channel.id == 794908427809062912:
+        if ctx.channel.id != 699762298721665158:
+            return
+        
+        await ctx.message.delete()
 
-            # Initialise variables
-            start_from = None
-            limited = False
-            previous_message = "0 0"
-            count = 0
-            delete_after = None
+        # Initialise variables
+        start_from = None
+        limited = False
+        previous_message = "0 0"
+        count = 0
+        delete_after = None
 
-            if len(args) > 0:
-                start_from = int(args[0])
-                limited = True
+        if len(args) > 0:
+            start_from = int(args[0])
+            limited = True
 
-            if len(args) > 1:
-                delete_after = int(args[1])
-                    
+        if len(args) > 1:
+            delete_after = int(args[1])
+                
+        # Iterate over list of all messages in counting-channel
+        async for m in ctx.channel.history(limit=None, oldest_first=True):
 
+            if limited:
+                count += 1
+                limited = count < start_from
 
+            if not limited:
+                pre = previous_message.replace("(", " ").split(" ")[0].strip()
+                pre = re.sub("[^0-9]", "", pre)
+                message_content = m.content
 
-
-            # Iterate over list of all messages in counting-channel
-            async for m in ctx.channel.history(limit=None, oldest_first=True):
-                try:
-                    if limited:
-                        count += 1
-                        if count >= start_from:
-                            limited = False
-                    else:
-                        pre = previous_message.replace("(", " ").split(" ")[0].strip()
-                        pre = re.sub("[^0-9]", "", pre)
-                        message_content = m.content
-                        if not message_content.startswith(str(int(pre)+1)):
-                            print("Bad count:", int(previous_message.split(" ")[0]), int(m.content.split(" ")[0]))
-                            await ctx.channel.send(m.jump_url, delete_after=delete_after)
-                        previous_message = message_content
-
+                try: 
+                    if not message_content.startswith(str(int(pre)+1)):
+                        print("Bad count:", int(previous_message.split(" ")[0]), int(m.content.split(" ")[0]))
+                        await ctx.channel.send(m.jump_url, delete_after=delete_after)
                 except ValueError:
                     pass
-                    
-            await ctx.message.delete()
+
+                previous_message = message_content
 
 
     @commands.Cog.listener()
@@ -70,19 +69,20 @@ class Counting(commands.Cog):
             return
 
         # Only checks messages in counting-channel
-        if message.channel.id == 699762298721665158:
+        if message.channel.id != 699762298721665158:
+            return
 
-            # gets the last 2 messages 
-            messages = await message.channel.history(limit=2).flatten()
+        # gets the last 2 messages 
+        messages = await message.channel.history(limit=2).flatten()
 
-            # Process the older message
-            pre = messages[1].content.replace("(", " ").split(" ")[0].strip()
-            pre = re.sub("[^0-9]", " ", pre)
+        # Process the older message
+        pre = messages[1].content.replace("(", " ").split(" ")[0].strip()
+        pre = re.sub("[^0-9]", " ", pre)
 
-            # Process the current message
-            message_content = messages[0].content
+        # Process the current message
+        message_content = messages[0].content
 
-            # Delete the current message if the number is wrong
-            if not message_content.startswith(str(int(pre)+1)):
-                await message.channel.send("Check yourself before you wreck yourself.", delete_after=3)
-                await message.delete()
+        # Delete the current message if the number is wrong
+        if not message_content.startswith(str(int(pre)+1)):
+            await message.channel.send("Check yourself before you wreck yourself.", delete_after=3)
+            await message.delete()
